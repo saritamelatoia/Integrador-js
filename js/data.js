5162465bfd54b3db6b52626b706bd3ba89eab88e
Sara <saracallerohotham2@gmail.com>
2023-11-15 19:41:44 -0300

    almostfinished

---
+++ js/data.js
@@ -0,0 +1,102 @@
+const productos = [
+    {
+        id: "anillo-01",
+        titulo: "Anillo 01",
+        imagen: "./img-productos/anillo1.jpg",
+        categoria: {
+            nombre: "Anillos",
+            id: "anillos",
+        },
+        precio: 30
+    },
+    {
+        id: "anillo-02",
+        titulo: "Anillo 02",
+        imagen: "./img-productos/anillo2.jpg",
+        categoria: {
+            nombre: "Anillos",
+            id: "anillos",
+        },
+        precio: 30
+    },
+    {
+        id: "aro-01",
+        titulo: "Aro 01",
+        imagen: "./img-productos/aro1.jpg",
+        categoria: {
+            nombre: "Aros",
+            id: "aros",
+        },
+        precio: 30
+    },
+    {
+        id: "aro-02",
+        titulo: "Aro 02",
+        imagen: "./img-productos/aro2.jpg",
+        categoria: {
+            nombre: "Aros",
+            id: "aros",
+        },
+        precio: 30
+    },
+    {
+        id: "aro-01",
+        titulo: "Aro 01",
+        imagen: "./img-productos/aro1.jpg",
+        categoria: {
+            nombre: "Aros",
+            id: "aros",
+        },
+        precio: 30
+    },
+    {
+        id: "collar-01",
+        titulo: "Collar 01",
+        imagen: "./img-productos/collar1.jpg",
+        categoria: {
+            nombre: "Collares",
+            id: "collares",
+        },
+        precio: 30
+    },
+    {
+        id: "collar-02",
+        titulo: "Collar 02",
+        imagen: "./img-productos/collar2.jpg",
+        categoria: {
+            nombre: "Collares",
+            id: "collares",
+        },
+        precio: 30
+    },
+    {
+        id: "reloj-01",
+        titulo: "Reloj 01",
+        imagen: "./img-productos/reloj1.jpg",
+        categoria: {
+            nombre: "Relojes",
+            id: "relojes",
+        },
+        precio: 30
+    },
+    {
+        id: "reloj-02",
+        titulo: "Reloj 02",
+        imagen: "./img-productos/reloj2.jpg",
+        categoria: {
+            nombre: "relojes",
+            id: "Relojes",
+        },
+        precio: 30
+    },
+    {
+        id: "reloj-03",
+        titulo: "Reloj 03",
+        imagen: "./img-productos/reloj3.jpg",
+        categoria: {
+            nombre: "Relojes",
+            id: "relojes",
+        },
+        precio: 30
+    },
+];
